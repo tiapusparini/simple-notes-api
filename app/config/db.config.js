5ad337db66cd26ada2803fba4584ef4743ab297b
tiapusparini <tia.pusparini@gmail.com>
2021-10-09 19:36:33 +0700

    change db connection

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,13 +1,13 @@
 module.exports = {
-    HOST: "localhost",
-    USER: "postgres",
-    PASSWORD: "pusparini24",
-    DB: "note_db",
-    dialect: "postgres",
-    pool: {
-      max: 5,
-      min: 0,
-      acquire: 30000,
-      idle: 10000
-    }
+  HOST: "ec2-34-205-14-168.compute-1.amazonaws.com",
+  USER: "otvvlntcfdwggy",
+  PASSWORD: "2cc728b356dea9799345163531844c87c8e335f06a27fcd43182040587f9fbb9",
+  DB: "d1dgb3tr5ms8ps",
+  dialect: "postgres",
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  }
 };
