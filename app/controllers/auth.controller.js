421f1cb64fb2fd0db20cc1e76f38e1667c8e325a
tiapusparini <tia.pusparini@gmail.com>
2021-12-07 15:33:23 +0700

    change expire time

--- app/controllers/auth.controller.js
+++ app/controllers/auth.controller.js
@@ -66,7 +66,7 @@ exports.login = async (req, res) => {
     };
 
     var token = jwt.sign(data, config.secret, {
-        expiresIn: 36000 // 10 hours
+        expiresIn: 86400 // 24 hours
     });
 
     const sendData = {
