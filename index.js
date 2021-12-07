fb16ae28aac20dbd53592dbabbf76a19421a8173
tiapusparini <tia.pusparini@gmail.com>
2021-12-07 15:34:19 +0700

    index

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(cors());
 // parse requests of content-type - application/json
 app.use(express.json());
 
-// parse requests of content-type - application/x-www-form-urlencoded
+// parse requests of content-type - application/x-www-form-urlencoded   
 app.use(express.urlencoded({ extended: true }));
 
 // simple route
